00ae906c3c70a49bab8bf7204f68f28d51cc1de8
arvindshinde521 <arvindshinde521@gmail.com>
2025-05-19 22:16:32 +0530

    Added getter and setter methods

--- src/GetterSetter/MainGetterSetterPrint.java
+++ src/GetterSetter/MainGetterSetterPrint.java
@@ -3,9 +3,22 @@ package GetterSetter;
 public class MainGetterSetterPrint {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-		System.out.println("keep");
+		gttereSetterClass obj = new gttereSetterClass();
+		obj.setCarmodel("KIA");
+		System.out.println("A car model is :" +obj.getCarmodel());
+		
+		obj.setCarColor("Balck");
+        System.out.println("Color of car is :"+ obj.getCarColor());	
+        
+        obj.setCarTire("MRF");
+        System.out.println("Tires of car is of :"+ obj.getCarTire());
+        
+        obj.setDoor(2);
+        System.out.println("Car has doors :"+obj.getDoor());
+        
+        obj.setIsconvertible(true);
+        System.out.println("car is setIsconvertible:" + obj.getIsConvertible());
+        
 	}
 
 }
--- src/GetterSetter/gttereSetterClass.java
+++ src/GetterSetter/gttereSetterClass.java
@@ -1,11 +1,46 @@
 package GetterSetter;
 
 public class gttereSetterClass {
+	
+	private String carModel = "Suzuki";
+	private String carColor = "White";
+	private String carTire = "Bridgestone";
+	private int door = 4;
+	private boolean isConvertible = true;
+	
+	public String getCarmodel() {
+		return carModel;
+	}
+	public String getCarColor() {
+		return carColor;
+	}
+	public String getCarTire() {
+		return  carTire;
+	}
+	public int getDoor() {
+		return door;
+	}
+	public boolean getIsConvertible() {
+		return  isConvertible;
+	}
+	public void setCarmodel(String carModel) {
+		this.carModel = carModel;
+	}
+	public void setCarColor(String carColor) {
+		this.carColor = carColor;
+	}
+	public void setCarTire(String carTire) {
+		this.carTire = carTire;
+	}
+	public void setDoor(int door) {
+		this.door=door;
+	}
+	public void setIsconvertible(boolean isConvertible) {
+		this.isConvertible = isConvertible;
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-		System.out.println("i Am arvind");
+		
 	}
 
 }
