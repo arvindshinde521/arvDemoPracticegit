fc80f0c315af8a4833ab6f41e1114ca8770100b3
arvindshinde521 <arvindshinde521@gmail.com>
2025-05-19 14:41:38 +0530

    I have a new code

---
+++ src/GetterSetter/MainGetterSetterPrint.java
@@ -0,0 +1,11 @@
+package GetterSetter;
+
+public class MainGetterSetterPrint {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("keep");
+	}
+
+}
---
+++ src/GetterSetter/gttereSetterClass.java
@@ -0,0 +1,11 @@
+package GetterSetter;
+
+public class gttereSetterClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("i Am arvind");
+	}
+
+}
