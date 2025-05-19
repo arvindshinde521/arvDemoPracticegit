df907cc7b8ed03f8d23251eee5f99f665d952983
arvindshinde521 <arvindshinde521@gmail.com>
2025-05-19 15:06:53 +0530

    added new string operations

---
+++ src/StringPackage/StringClass.java
@@ -0,0 +1,18 @@
+package StringPackage;
+
+public class StringClass {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	String name ;
+	
+	String nm = new String("arvind");
+	
+	System.out.println(nm.charAt(0));
+	System.out.println(nm.length());
+	System.out.println(nm.charAt(2));
+
+	}
+
+}
